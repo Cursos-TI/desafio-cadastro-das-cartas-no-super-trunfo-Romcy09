0a56ea5df162c3d3917dcb61daed065eb138cde0
Romcy09 <romcytrabalhos@gmail.com>
2025-02-24 16:01:40 -0300

    Update supertrunfo.c

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,12 +1,52 @@
-   #include <stdio.h>
+  #include <stdio.h>
 
-   int main(){
+int main()
+{
 
-      printf("Desafio Super Trunfo!\n");
+    printf("Desafio Super Trunfo!\n");
 
-      printf("")
+    char Estado;
+    char CodigoCarta[8];
+    char Cidade[50];
+    int População;
+    float Area;
+    float PIB;
+    int PontosTuristicos;
+
+    printf("Registro da Carta 1: \n");
+
+    printf("Digite a Letra do Estado: ");
+    scanf("%c", &Estado);
+
+    printf("Digite o Código: ");
+    scanf("%s", &CodigoCarta);
+
+    printf("Digite o Nome da Cidade: ");
+    scanf("%s", &Cidade);
+
+    printf("Digite a População: ");
+    scanf("%d", &População);
+
+    printf("Digite o Valor da Área em km²: ");
+    scanf("%d", &Area);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &PIB);
+
+    printf("Digite o Número de Pontos Turisticos: ");
+    scanf("%d", &PontosTuristicos);
+
+    return 0;
+}
+
+      
 
 
+         
+
+      
+      
+      
 
 
 
